4466fc58b3cedcae25a08b8a310ecf291fe066d2
ducky961 <139243352+ducky961@users.noreply.github.com>
2026-08-28 21:26:19 +0900

    Quiz_2

--- practice.py
+++ practice.py
@@ -1,32 +1,17 @@
-print(1 + 1) # 2, 덧셈
-print(3 - 2) # 1, 뺄셈
-print(5 * 2) # 10, 곱셈
-print(6 / 3) # 2, 나눗셈
+#quiz 2
+#당신은 최근에 코딩 스터디 모임을 새로 만들었습니다.
+#월 4회 스터디를 하는데 3번은 온라인으로 하고 1번은 
+#오프라인으로 하기로 했습니다.
 
-print(2 ** 3) # 2^3 = 8, 제곱
-print(5 % 3) # 2, 나머지 구하기
-print(10 % 3) # 1, 나머지 구하기
-print(5 // 3) # 3, 몫 구하기
-print(10 // 3) # 3, 몫 구하기
+#아래 조건에 맞는 오프라인 모임 날짜를 정해주는 프로그램을 작성하시오.
+#조건 1 : 랜덤으로 날짜를 뽑아야 함
+#조건 2 : 월별 날짜는 다름을 감안하여 최소 일수인 28 이내로 정함
+#조건 3 : 매월 1~3일은 스터디 준비를 해야 하므로 제외
 
-print(10 > 3) # True
-print(4 >= 7) # False
-print(10 < 3) # False
-print(5 <= 5) # True
+#출력문 예제
+#오프라인 스터디 모임 날짜는 매월 x일로 선정되었습니다.
 
-print(3 == 3) # True
-print(4 == 2) # False
-print(3 + 4 == 7) # True
+from random import *
 
-# not 연산자
-print(1 != 3) # True
-print(not(1 != 3)) # false
-# and 연산자
-print((3 > 0) and (3 < 5)) # True
-print((3 > 0) & (3 < 5)) # True
-# or 연산자
-print((3 > 0) or (3 > 5)) # True
-print((3 > 0) | (3 > 5)) # True
-# 응용
-print(5 > 4 > 3) # True
-print(5 > 4 > 7) # False
+date = randint(4,28)
+print("오프라인 스터디 모임 날짜는 매월" + str(date) + "일로 선정되었습니다.")
