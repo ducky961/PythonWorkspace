9fcf6330a1c892186fdf80b84642c76491870d45
ducky961 <139243352+ducky961@users.noreply.github.com>
2026-08-28 21:20:08 +0900

    숫자 처리 함수

---
+++ 숫자 처리 함수.py
@@ -0,0 +1,9 @@
+print(abs(-5)) # 5, abs = 절대값
+print(pow(4,2)) # 4^2 = 4*4 = 16, pow = 제곱
+print(max(5,12)) # 12, max = 최대값
+print(min(5,12)) # 5, min = 최소값
+print(round(3.14)) # 3, round = 반올림
+print(round(4.99)) # 5
+# floor = 내림
+# ceil = 올림
+# sqrt = 제곱근
