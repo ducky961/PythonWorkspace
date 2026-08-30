732eaee5b0ec019f11b9b6280a14db5ef0ef9383
ducky961 <139243352+ducky961@users.noreply.github.com>
2026-08-30 19:25:11 +0900

    Create 2-5. 탈출문자.py

---
+++ 2-5. 탈출문자.py
@@ -0,0 +1,18 @@
+# \n : 줄바꿈
+print("백문이 불여일견\n백견이 불여일타")
+
+# 원하는 출력물 : 저는 "파이썬"입니다.
+#print('저는 "파이썬"입니다.') # 되기는 됨, 형식이 헷갈릴 수 도 있음
+print("저는 \"파이썬\"입니다.")
+
+# \\ : 문장 내에서 \를 표현하고 싶을 때 (2번 입력)
+print("C:\\Users\\Ori\\Desktop\\PythonWorkspace")
+
+# \r : 커서를 맨 앞으로 이동
+print("Red Apple\rPine") # PineApple
+
+# \b : 백스페이스 (한 글자 삭제)
+print("Redd\bApple") # RedApple
+
+# \t : 탭
+print("Red Apple\tPine") # Red Apple    Pine
